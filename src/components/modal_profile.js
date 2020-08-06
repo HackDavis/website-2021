ff64637dddf5d65bcec30fe6bbbc202fb1b4ea84
benank <terkolskie@gmail.com>
2020-08-05 21:02:03 -0700

    Progress
    Co-authored-by: Omid Mogasemi <omidmogasemi@gmail.com>
    Co-authored-by: Alex Long <alelong@ucdavis.edu>
    Co-authored-by: NickSulistio <NickJSulistio@gmail.com>

--- src/components/modal_profile.js
+++ src/components/modal_profile.js
@@ -1,16 +1,42 @@
 import React, { useEffect } from 'react';
 import Modal from "./modal"
 import styles from "./css/modal_profile.module.css"
-
+import { getUser } from "../utils/auth"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // User's interest, technologies, application status, teams, 
 
 const ProfileModal = ( props ) => {
-    
+    const user = getUser();
+    const { displayName, email } = user;
+
     return(
         <Modal {...props} id="profilemodal">
             <div className={styles.modal}>
-                <h1>Profile</h1>
+                <Container fluid p-0 noGutters={true}>
+                    <Row lg={12}>
+                        <Col xs={12} noGutters={true}>
+                            <div class={`${styles.title} ${styles.red}`}>Hello, {displayName}!</div>
+                        </Col>
+                    </Row>
+                    <Row lg={6}>
+                        <Col xs={6} noGutters={true}>
+                            <Col>
+                                <div className={styles.applicationcontainer}>Application Container</div>
+                            </Col>
+                            <Col>
+                                <div className={styles.teamfindercontainer}>Team Finder Container</div>
+                            </Col>
+                        </Col>
+                    </Row>
+                    <Row lg={6}>
+                        <Col xs={6}>
+                            <div className={styles.badgescontainer}>Badges Container</div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </Modal>
             
