860910c3325a0b2b2c808b911ff2a217b80dd6c3
Alex <alelong@ucdavis.edu>
2020-10-07 23:23:19 -0700

    Added cloud image

--- src/components/section_stats.js
+++ src/components/section_stats.js
@@ -2,16 +2,22 @@ import React from "react"
 import { Link } from "gatsby"
 
 import styles from "./css/section_stats.module.css"
+import cloudImage from "../images/cloud.svg"
 
 
 const StatsSection = () => {
     return (
         <div className={styles.background}>
+            <div className={styles.cloud}>
+                <img src={cloudImage}/>
+            </div>
             <div className={styles.statscontainer}>
-                600+ HACKERS<br></br>
-                24 HOURS<br></br>
-                100+ PROJECTS<br></br>
-                $25,000+ PRIZES<br></br><br></br>
+                <div className={styles.stats}>
+                    600+ HACKERS<br></br>
+                    24 HOURS<br></br>
+                    100+ PROJECTS<br></br>
+                    $25,000+ PRIZES<br></br>
+                </div>
                 <div className={styles.viewWinner}>
                     <a href="https://hackdavis2020.devpost.com/" target="_blank">VIEW 2020 WINNERS</a>
                 </div>
