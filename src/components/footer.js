99f46a8d7625570e58f14a5bb12a11af349040ca
benank <terkolskie@gmail.com>
2020-10-27 18:28:29 -0700

    Update dates

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className={styles.footerstyle}>
             <div className="container-fluid-padding">
-                <h5>&copy; HackDavis 2020</h5>
+                <h5>&copy; HackDavis 2021</h5>
             </div>
         </div>
     )
