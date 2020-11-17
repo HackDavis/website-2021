299ff109edee304bbd80108ffe48ff73b6e58f87
benank <terkolskie@gmail.com>
2020-11-17 10:56:18 -0800

    Updates
    Co-authored-by: Alex Long <alelong@ucdavis.edu>

--- src/components/cows/cow_handler.js
+++ src/components/cows/cow_handler.js
@@ -9,6 +9,11 @@ const CowHandler = (props) => {
     const [myId, setMyId] = useState();
     const [cows, setCows] = useState({});
 
+    useEffect(() => 
+    {
+        SocketHandler
+    }, true)
+
     return <>
         {<SocketHandler cow={cows} setCows={setCows} myId={myId} setMyId={setMyId}></SocketHandler>}
         {Object.keys(cows).map((key) => {
@@ -17,3 +22,67 @@ const CowHandler = (props) => {
 };
 
 export default CowHandler
+
+
+/*
+
+
+const dotenv = require("dotenv")
+dotenv.config();
+
+const cows = {}
+
+let cow_id = 1;
+
+const io = require('socket.io')(process.env.PORT, {
+    cors: {
+        origin: ["http://localhost:8000", "http://localhost:8080", "https://hackdavis.io"],
+        credentials: true
+    },
+});
+
+function clamp(num, min, max)
+{
+    return Math.min(max, Math.max(num, min));
+}
+
+io.on('connect', client => {
+    console.log("connected " + client.id)
+
+    client.emit('set id', {id: client.id})
+    cows[client.id] = {name: `Cow ${cow_id++}`, pos: {x: Math.random(), y: Math.random()}}
+
+    client.emit('sync cows', cows);
+
+    client.on('move cow', (args) => 
+    {
+        console.log("move cow")
+        console.log(args)
+        if (args.pos == undefined || args.pos.y == undefined) {return;}
+        if (cows[client.id] == undefined) {return;}
+
+        cows[client.id].pos = {x: clamp(args.pos.x, 0, 1), y: clamp(args.pos.y, 0, 1)};
+        io.emit('sync cow', {id: client.id, cow: cows[client.id]});
+        console.log('sync')
+    })
+
+    client.on('disconnect', () => {
+        delete cows[client.id];
+        console.log("disconnect " + client.id);
+    });
+});
+
+console.log("Listening on port " + process.env.PORT)
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- src/components/cows/socket.js
+++ src/components/cows/socket.js
@@ -7,34 +7,35 @@ const SocketHandler = (props) => {
 
     const [socket, setSocket] = useState(openSocket('http://localhost:8080'));
 
-    socket.on('connect', () => 
-    {
-        console.log("connected")
-    })
-
-    socket.on('set id', (args) => 
-    {
-        props.setMyId(args.id);
-    })
-
-    socket.on('sync cows', (sync_cows) => 
-    {
-        props.setCows(sync_cows);
-    })
-  
-    socket.on('sync cow', (args) => 
-    {
-        const cows_copy = JSON.parse(JSON.stringify(props.cows || {}));
-        cows_copy[args.id] = args.cow;
-        props.setCows(cows_copy);
-    })
-
     useEffect(() => {
 
+        console.log("SocketHandler render")
         $(window).on("click", function (e) {
             socket.emit('move cow', {
                 pos: {x: e.pageX / $(window).width(), y: e.pageY / $(window).height()}
             })
+        })
+
+        socket.on('connect', () => 
+        {
+            console.log("connected")
+        })
+
+        socket.on('set id', (args) => 
+        {
+            props.setMyId(args.id);
+        })
+
+        socket.on('sync cows', (sync_cows) => 
+        {
+            props.setCows(sync_cows);
+        })
+    
+        socket.on('sync cow', (args) => 
+        {
+            const cows_copy = JSON.parse(JSON.stringify(props.cows || {}));
+            cows_copy[args.id] = args.cow;
+            props.setCows(cows_copy);
         })
 
         // Cleanup event handlers
@@ -42,10 +43,14 @@ const SocketHandler = (props) => {
             // clean up the event handler when the component unmounts
             $(window).off("click")
             socket.disconnect();
+            socket.off('connect');
+            socket.off('set id');
+            socket.off('sync cows');
+            socket.off('sync cow');
         }
-    })
+    }, true)
 
-    return <></>
+return <></>
 }
 
 export default SocketHandler
