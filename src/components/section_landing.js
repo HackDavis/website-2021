7f2482593a3c2dc6680ae3cadf45bb80df7d7d2c
benank <terkolskie@gmail.com>
2020-11-18 19:40:13 -0800

    Fix error

--- src/components/section_landing.js
+++ src/components/section_landing.js
@@ -58,7 +58,7 @@ const LandingSection = () => {
                     <div className={styles.start_button_text}>START</div>
                     <div className={styles.start_button_button}>
                         <svg width="20" height="13" viewBox="0 0 20 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2 2L10 10L18 2" stroke="white" stroke-width="3"/>
+                            <path d="M2 2L10 10L18 2" stroke="white" strokeWidth="3"/>
                         </svg>
                     </div>
                 </div>
