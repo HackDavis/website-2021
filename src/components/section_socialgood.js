93a3197e72a9131c6e58177dbe452703acd1166a
benank <terkolskie@gmail.com>
2020-11-19 14:55:21 -0800

    Fix social good

--- src/components/section_socialgood.js
+++ src/components/section_socialgood.js
@@ -7,7 +7,7 @@ const SocialGoodSection = (props) => {
     return (
         <div className="container-fluid p-0">
             <div className={`row no-gutters align-items-center ${styles.background}`}>
-                <div className={`col-10 offset-1 col-md-4 offset-md-1 order-2 order-md-1 ${styles.description} ${props.fadeSocialGood && styles.slideinright}`}>
+                <div className={`col-10 offset-1 col-md-4 offset-md-1 order-2 order-md-1 ${styles.description} ${props.fadeSocialGood && styles.slideinleft}`}>
                     <div className= {styles.title}>
                         Hack for Social Good
                     </div>
@@ -18,7 +18,7 @@ const SocialGoodSection = (props) => {
                     On January 16-17, over 700 students, hackers, and creators will come together virtually for 36 hours of hacking. 
                     For the 6th year in a row, we're bringing the most talented students in California (and beyond!) to address the world’s most pressing issues.
                 </div>
-                <div className={`${styles.social_good_container} ${props.fadeSocialGood && styles.slideinleft} col-10 offset-1 col-md-5 offset-md-1 order-1 order-md-2`}>
+                <div className={`${styles.social_good_container} ${props.fadeSocialGood && styles.slideinright} col-10 offset-1 col-md-5 offset-md-1 order-1 order-md-2`}>
                     <img className={styles.image} src="https://filmdaily.co/wp-content/uploads/2020/05/cat-memes-lede.jpg"></img>
                 </div>
             </div>
