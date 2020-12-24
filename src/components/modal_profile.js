da79d673e5435f562308e264a6d1756670ba71a9
benank <terkolskie@gmail.com>
2020-12-23 15:12:32 -1000

    Update icon

--- src/components/modal_profile.js
+++ src/components/modal_profile.js
@@ -77,7 +77,7 @@ const ProfileModal = props => {
     function RSVPResponse(response) {
         let db = userStatus.db
         let userChoice = response ? "Yes" : "No"
-        console.log(response);
+        // console.log(response);
 
         let new_badges;
         new_badges = JSON.parse(JSON.stringify(userStatus.badges))
@@ -249,9 +249,17 @@ const ProfileModal = props => {
                         </div> }
                         {showOthers && <hr></hr>}
                         <div className={styles.teamfindercontainer} style={{height: teamFinderHeight}}>
-                            <div className={styles.team_finder}>
+                            <div className={styles.team_finder} onClick={changeTeamFinderSize}>
                                 Team Finder
-                                <img className={styles.expand_button} onClick={changeTeamFinderSize} src={images['teamfinder_size']} />
+                                <div className={styles.expand_button} onClick={changeTeamFinderSize}>
+                                {!showOthers ? 
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-fullscreen-exit" viewBox="0 0 16 16">
+                                        <path d="M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z"/>
+                                    </svg> :
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-fullscreen" viewBox="0 0 16 16">
+                                        <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z"/>
+                                    </svg>}
+                                </div>
                             </div>
                             <div className={styles.modalsectioncontent} id="content">
                                 <TeamFinder
