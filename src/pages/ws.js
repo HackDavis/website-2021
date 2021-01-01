db38f87fc97d1ee344a39ef51ef5a7e1ad1ad2df
benank <terkolskie@gmail.com>
2020-12-31 15:03:26 -1000

    Add workshop attendance page

---
+++ src/pages/ws.js
@@ -0,0 +1,16 @@
+import "../components/css/clear.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { useEffect } from "react"
+
+const WSPage = () => {
+  
+    useEffect(() => 
+    {
+        window.location = `https://hackdavis.typeform.com/to/in2WXRm3`
+    })
+
+    return <></>
+}
+
+export default WSPage
