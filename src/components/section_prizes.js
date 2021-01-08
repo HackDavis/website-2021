dffda1a17ac480a33bbb0139203e6944b5164372
benank <terkolskie@gmail.com>
2021-01-07 20:16:14 -1000

    Add more prizes

--- src/components/section_prizes.js
+++ src/components/section_prizes.js
@@ -11,7 +11,8 @@ const prizes1 =
     "Best Beginner Hack": "This must be every team member's first hackathon! Each member of the winning team gets a Smart TV.",
     "Best Interdisciplinary Hack": "At least one member of your team must not be a Computer Science (or similar) major! Each member of the winning team gets a Fitbit.",
     "Best Global Hack": "Best Global Hack goes to the team that offers a way to address one or more of the 17 <a href='https://www.un.org/sustainabledevelopment/sustainable-development-goals/' style={{'textDecoration': 'underline'}}>UN Sustainable Development Goals</a> and goes to the best project by a team of hackers from multiple (at least 2) time zones! Each member of the winning team gets an Amazon Kindle.",
-    "DigitalOcean Credits": "DigitalOcean credits will be given to the winning teams for Best Hack for Social Good, Best Beginner Hack, and Best Interdisciplinary Hack. Each member of each winning team gets $125 in DigitalOcean credits.",
+    "Best Domain Registered with Domain.com": "Register a .tech, .space, or .online domain name using <a href='https://domain.com'>Domain.com</a> during the weekend. Each team may submit one entry per person on the team. Each winning team member will receive a PowerSquare Qi Wireless Phone Charger & Domain.com Backpack.",
+    "Best Use of Google Cloud": "Build your hackathon project with a suite of secure storage, powerful compute, and integrated data analytics products provided by Google Cloud. See full list of products here: <a href='https://g.co/cloud'>g.co/cloud</a>. Each winning team member will receive a Google Branded Parkland Academy Backpack.",
 }
 
 const prizes2 = 
@@ -19,7 +20,9 @@ const prizes2 =
     "Best Use of TensorFlow.js": "Use TensorFlow.js in your project! Each member of the winning team gets a Pixel 4A smart phone.",
     "Best Use of Twilio API": "Use the Twilio API in your project! Each member of the winning team gets a Logitech Webcam.",
     "Best Use of Confluent Platforms": "Use Confluent Platforms in your project! Winning team gets a $500 Amazon Gift Card.",
-    "Best Pitch with Contrary": "Attend the Contrary event on Sunday to be eligible. Winning team gets a $500 cash prize."
+    "DigitalOcean Credits": "DigitalOcean credits will be given to each member of the winning teams for Best Hack for Social Good ($250 each), Best Beginner Hack ($125 each), and Best Interdisciplinary Hack ($125 each).",
+    "Best Hardware Hack Sponsored by Digi-Key": "Using your preferred hardware or hardware emulator, build a hack for your chance to win a Grove Beginner Kit, with Arduino Uno R3 included. We select two winning teams for this category. Each winning team member will receive a prize!",
+    "Best Use of DataStax Astra": "Apache Cassandra is one of the most widely used database technologies in modern computing with a proven track record of scalability. Just ask Apple, Netflix and eBay. Build a hack using DataStax Astra’s Cassandra-made-easy service and gain in-demand skills along with a chance to win Blue Snowball Microphones for you and your whole team. It’s easy to sign up with 5GB free and no credit card needed. Start becoming a Cassandra expert today!"
 }
 
 const PrizeSection = () => {
