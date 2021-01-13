f60b7cea8334672771c88d8db89b68b8e6795e89
Alex <alelong@ucdavis.edu>
2021-01-13 15:11:09 -0800

    redirect to first meal typeform

---
+++ src/pages/first_meal.js
@@ -0,0 +1,15 @@
+import "../components/css/clear.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from "react"
+
+const FirstMeal = () =>{
+    
+    useEffect(()=> {
+
+        window.location = `https://hackdavis.typeform.com/to/wxciBddM`
+    })
+
+    return <></>
+}
+
+export default FirstMeal
