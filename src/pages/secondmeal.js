0b4f9fe0588f540f6eeae829a86bd5a80f02dd81
Alex <alelong@ucdavis.edu>
2021-01-13 15:44:49 -0800

    added second and third meal

---
+++ src/pages/secondmeal.js
@@ -0,0 +1,15 @@
+import "../components/css/clear.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from "react"
+
+const SecondMeal = () =>{
+    
+    useEffect(()=> {
+
+        window.location = `https://hackdavis.typeform.com/to/qE25tVJJ`
+    })
+
+    return <></>
+}
+
+export default SecondMeal
---
+++ src/pages/thirdmeal.js
@@ -0,0 +1,15 @@
+import "../components/css/clear.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from "react"
+
+const ThirdMeal = () =>{
+    
+    useEffect(()=> {
+
+        window.location = `https://hackdavis.typeform.com/to/VVxR9URV`
+    })
+
+    return <></>
+}
+
+export default ThirdMeal
