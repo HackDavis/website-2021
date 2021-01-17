6aadb7b177c0cf62f2e5a622306c2ce04b39fb47
benank <terkolskie@gmail.com>
2021-01-17 14:20:37 -0800

    Add page

---
+++ src/pages/meal5.js
@@ -0,0 +1,15 @@
+import "../components/css/clear.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from "react"
+
+const FifthMeal = () =>{
+    
+    useEffect(()=> {
+
+        window.location = `https://hackdavis.typeform.com/to/yjhy2fif`
+    })
+
+    return <></>
+}
+
+export default FifthMeal
