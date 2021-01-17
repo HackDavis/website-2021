020875dcaf7a6626af5998a32ebd8ab16dae1dc5
benank <terkolskie@gmail.com>
2021-01-17 11:23:41 -0800

    Add page

---
+++ src/pages/meal4.js
@@ -0,0 +1,15 @@
+import "../components/css/clear.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from "react"
+
+const FourthMeal = () =>{
+    
+    useEffect(()=> {
+
+        window.location = `https://hackdavis.typeform.com/to/TL7MsStD`
+    })
+
+    return <></>
+}
+
+export default FourthMeal
