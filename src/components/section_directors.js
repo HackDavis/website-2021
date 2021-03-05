e51ee55c0910edb17b5327520dde9a55196e5222
Nick Sulistio <njsulistio@ucdavis.edu>
2021-03-04 22:25:38 -0800

    resolved merge conflicts by accepting my changes

--- src/components/section_directors.js
+++ src/components/section_directors.js
@@ -35,7 +35,6 @@ const DirectorsSection = () => {
     }
 
 
-<<<<<<< HEAD
     const directorInfo = 
         {
         "omid": {name:"Omid", description: `Technical Lead`, linkedin: `https://www.linkedin.com/in/omidmogasemi/`},
@@ -57,29 +56,6 @@ const DirectorsSection = () => {
         }
         return "3rem";
     }
-=======
-    // const directorInfo = 
-    //     {
-    //     "Omid": {description: `Technical Lead`, linkedin: `https://www.linkedin.com/in/omidmogasemi/`},
-    //     "Vivek": {description: `Sponsorship Lead`, linkedin: `https://www.linkedin.com/in/vivekshome/`},
-    //     "Cheryl": {description: `Design Lead`, linkedin: `https://www.linkedin.com/in/cherylcai/`},
-    //     "Stephenie": {description: `Finance Lead`, linkedin: `https://www.linkedin.com/in/stepheniecho/`},
-    //     "Joyce": {description: `External Lead`, linkedin: `https://www.linkedin.com/in/joyce-lu-b8b96b197/`},
-    //     "Abhishek": {description: `Operations Co-Lead`, linkedin: `https://www.linkedin.com/in/abhishekhandigol/`},
-    //     "Ishani": {description: `Operations Co-Lead`, linkedin: `https://www.linkedin.com/in/ishani-pandya/`},
-    //     "Alex": {description: `Technical`, linkedin: `https://www.linkedin.com/in/alelong/`},
-    //     "Nick": {description: `Technical`, linkedin: `https://www.linkedin.com/in/nickjsulist/`},
-    //     "Sivani": {description: `Sponsorship`, linkedin: `https://www.linkedin.com/in/sivani-voruganti/`},
-    //     "Shachi": {description: `Sponsorship`, linkedin: `https://www.linkedin.com/in/shachichampaneri/`},
-    //     "Barno": {description: `Sponsorship`, linkedin: `https://www.linkedin.com/in/sabarno/`},
-    //     }
-    // function adjustSizing(index) {
-    //     if (index == Object.keys(directorInfo).length - 1) {
-    //         return "22rem";
-    //     }
-    //     return "3rem";
-    // }
->>>>>>> master
 
     const images = GetImageMap();
 
@@ -89,88 +65,9 @@ const DirectorsSection = () => {
                 <div className={'col-10 col-md-8 offset-1 offset-md-2'}>
                     <div className={styles.headerText}>Directors</div>
                     <div className={`row no-gutters`}>
-<<<<<<< HEAD
                         {Object.keys(directorInfo).map((key, index) => {
                             return <DirectorBlurb sizing={adjustSizing(index)}directorImage={images[key]} directorName={directorInfo[key].name} directorDesc={directorInfo[key].description} linkedin={directorInfo[key].linkedin}></DirectorBlurb>;
                         })}
-=======
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "3rem"}}>
-                            <div>
-                                <img src={images["omid"]} className={styles.headshot} style={{borderRadius: "10px"}} onClick={() => window.open("https://www.linkedin.com/in/omidmogasemi/")}/>
-                                <h6 className={styles.title}><br />Omid<br />Co-President<br />Lead Technical</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "3rem"}}>
-                            <div>
-                                <img src={images["vivek"]} className={styles.headshot} style={{borderRadius: "10px"}} onClick={() => window.open("https://www.linkedin.com/in/vivekshome/")}/>
-                                <h6 className={styles.title}><br />Vivek<br />Co-President<br />Lead Sponsorship</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "3rem"}} onClick={() => window.open("https://www.linkedin.com/in/cherylcai/")}>
-                            <div>
-                                <img src={images["cheryl"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Cheryl<br />Lead Design</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "3rem"}} onClick={() => window.open("https://www.linkedin.com/in/stepheniecho/")}>
-                            <div>
-                                <img src={images["stephenie"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Stephenie<br />Lead Finance</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`row no-gutters`}>
-                    <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "3rem"}} onClick={() => window.open("https://www.linkedin.com/in/joyce-lu-b8b96b197/")}>
-                            <div>
-                                <img src={images["joyce"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Joyce<br />Lead External</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "1.25rem"}} onClick={() => window.open("https://www.linkedin.com/in/abhishekhandigol/")}>
-                            <div>
-                                <img src={images["abhishek"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Abhishek<br />Co-Lead Operations</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "1.25rem"}} onClick={() => window.open("https://www.linkedin.com/in/ishani-pandya/")}>
-                            <div>
-                                <img src={images["ishani"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Ishani<br />Co-Lead Operations</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "1.25rem"}} onClick={() => window.open("https://www.linkedin.com/in/alelong/")}>
-                            <div>
-                                <img src={images["alex2"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Alex<br />Technical</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`row no-gutters`}>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "1.25rem"}} onClick={() => window.open("https://www.linkedin.com/in/nickjsulist/")}>
-                            <div>
-                                <img src={images["nick"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Nick<br />Technical</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "1.25rem"}} onClick={() => window.open("https://www.linkedin.com/in/sivani-voruganti/")}>
-                            <div>
-                                <img src={images["sivani"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Sivani<br />Sponsorship</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "1.25rem"}} onClick={() => window.open("https://www.linkedin.com/in/shachichampaneri/")}>
-                            <div>
-                                <img src={images["shachi"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Shachi<br />Sponsorship</h6>
-                            </div>
-                        </div>
-                        <div className={'col-12 col-md-3 membercontainer'} style={{marginBottom: "22rem"}} onClick={() => window.open("https://www.linkedin.com/in/sabarno/")}>
-                            <div>
-                                <img src={images["barno"]} className={styles.headshot} style={{borderRadius: "10px"}} />
-                                <h6 className={styles.title}><br />Barno<br />Sponsorship</h6>
-                            </div>
-                        </div>
->>>>>>> master
                     </div>
                 </div>
             </div>
